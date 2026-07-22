08ca13d632e2b4eeff774fccc2b5cd2fa80cef4c
sundaevy <sundaevyy@gmail.com>
2026-07-22 14:03:26 +0300

    basic frontend setup with servce files, add target to gitignore, add cors config

---
+++ frontend/types/index.ts
@@ -0,0 +1,26 @@
+export type Tier = 'Platinum' | 'Gold' | 'Silver' | 'Bronze' | 'Borked';
+
+export interface GameDto {
+  id: number;
+  steamAppid: number;
+  name: string;
+  createdAt: string;
+}
+
+export interface ReportDto {
+  id: number;
+  user_id: number;
+  username: string;
+  game_id: number;
+  gameName: string;
+  tier: Tier;
+  distribution: string;
+  createdAt: string;
+}
+
+export interface UserDto {
+  id: number;
+  username: string;
+  email: string;
+  createdAt: string;
+}
