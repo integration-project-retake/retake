77830db0f0aaa73ea08410d073cdbf20470f8344
EnisChavushUCLL <r1031870@ucll.be>
2026-08-11 16:22:17 +0200

    Added Thematic Selections and Implemented Scenic Theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,25 +1,33 @@
 import { AuthProvider } from '@/context/AuthContext';
 import { LanguageProvider } from '@/context/LanguageContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
-import Footer from '@/components/Footer';
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
 
+
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
 
+
 export const metadata: Metadata = {
   title: 'ProtonDB Clone',
   description: 'Linux game compatibility reports',
 };
+
 
 export default function RootLayout({
   children,
@@ -29,16 +37,25 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
+
         <AuthProvider>
           <LanguageProvider>
-            <Header />
-            {children}
+            <ThemeProvider>
+
+              <Header />
+
+              {children}
+
+              <Footer />
+
+            </ThemeProvider>
           </LanguageProvider>
         </AuthProvider>
-        <Footer />
+
       </body>
     </html>
   );
